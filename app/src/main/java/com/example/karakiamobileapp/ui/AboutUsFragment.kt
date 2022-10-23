89795235cab121f3a8f9f4fa0dd06c7ae7316701
Reginald Abellana <71242306+SapphireReg@users.noreply.github.com>
2022-10-23 14:28:06 +1300

    AboutUsAddition
    Added about us xml, fragment, and group photo.

---
+++ app/src/main/java/com/example/karakiamobileapp/ui/AboutUsFragment.kt
@@ -0,0 +1,15 @@
+package com.example.karakiamobileapp.ui
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.karakiamobileapp.R
+
+class AboutUsFragment : Fragment(R.layout.fragment_about_us) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
