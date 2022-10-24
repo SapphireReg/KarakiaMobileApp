b8e97bd553004f5144fc552c1c98b862a5fff800
Reginald Abellana <71242306+SapphireReg@users.noreply.github.com>
2022-10-24 18:47:20 +1300

    Changed Color Theme DialogBox
    changed the dialogbox button color through themes.xml

--- app/src/main/java/com/example/karakiamobileapp/MainActivity.kt
+++ app/src/main/java/com/example/karakiamobileapp/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import androidx.annotation.NonNull
 import androidx.core.content.edit
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
